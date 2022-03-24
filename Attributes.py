819f3c5c7bad1273cfd864713b65ab1209ee2923
vaunparbhakar <varunparbhakar@yahoo.in>
2022-03-23 19:00:08 -0700

    A new parser has been implemented for more efficient string data grabbing. As of now, FREE CASH FLOW, OPERATING CASH FLOW, CapitalExpenditure are working, now I will begin working on revenue and other data attributes.

---
+++ Attributes.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Attribute(Enum):
+    FREE_CASH_FLOW = 0
+    OPERATING_CASH_FLOW = 1
+    CAPITAL_EXPENDITURE = 2
+
--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,5 @@
 import json
+from Attributes import Attribute
 
 from selenium import webdriver
 
@@ -50,41 +51,196 @@ class Scraper:
         # Getting the main Class = "D(tbrg))
         webElement = self.driver.find_element_by_xpath("//*[@id=\"Col1-1-Financials-Proxy\"]/section/div[4]/div[1]/div[1]/div[2]")
 
+
+        freeCashFlowList = self.stringParser(webElement.text, Attribute.FREE_CASH_FLOW)
+        print("Free Cash flow of", self.TICKER, "is", freeCashFlowList)
+
+    def revenue(self):
+        self.driver.get("https://finance.yahoo.com/quote/{}/financials?p={}".format(self.TICKER, self.TICKER))
+        self.driver.implicitly_wait(5)
+
+
+        # Working Free cash flow Address 10:56 PM 22 March "//*[@id=\"Col1-1-Financials-Proxy\"]/section/div[4]/div[1]/div[1]/div[2]"
+
+
+
+
+        # Getting the main Class = "D(tbrg))
+        webElement = self.driver.find_element_by_xpath("//*[@id=\"Col1-1-Financials-Proxy\"]/section/div[4]/div[1]/div[1]/div[2]")
         print(webElement.text)
-        freeCashFlowList = self.stringParser(webElement.text)
-        print(freeCashFlowList)
 
-    def stringParser(self, theString):
-        stringOffSet = 15
+        freeCashFlowList = self.stringParser(webElement.text, Attribute.FREE_CASH_FLOW)
+        print("Revenue of", self.TICKER, "is", freeCashFlowList)
+
+
+
+    def stringParser(self, theString, dataType):
+        """
+        Takes the stringBlock from the webElement and the dataType that the
+        user wants to extract and return a list with data from 5 periods
+        such as [TTM, CURRENT YEAR, C_Y - 1, C_Y - 2, C_Y - 3]
+
+        EX: A String like : Free Cash Flow
+        101,853,000 92,953,000 -73,365,000 58,896,000 64,121,000
+
+        will return [101853000, 92953000, -73365000, 58896000, 64121000]
+        :param theString:
+        :param dataType:
+        :return:
+        """
         myStringList = list(theString)
-        currentCounter = 0
-        freeAt = 0
-        for el in myStringList:
-            character = "" + el
-            if (character.isalpha()):
-                if (myStringList[currentCounter] == "F"
-                        and myStringList[currentCounter + 1] == "r"
-                        and myStringList[currentCounter + 2] == "e"
-                        and myStringList[currentCounter + 3] == "e"):
-                    print("found free")
-                    freeAt = currentCounter
-                    break
-            currentCounter += 1
 
-        print(myStringList[freeAt + stringOffSet:])
+        solutionList = None  # Storing the solution list
 
+        # Checking fot Free Cash Flow
+        if (dataType == Attribute.FREE_CASH_FLOW):
+            # The purpose of this offset is when we send the list to another method
+            # we want to make sure that the other method doesn't receive the word inside the list
+            stringOffSet = 15
+
+            currentCounter = 0
+
+            while (currentCounter < len(myStringList)):
+                character = "" + myStringList[currentCounter]
+                if (character.isupper()):
+                    if (self.freeCashFlowStringChecker(myStringList[currentCounter:])):
+                        # Only sending the part of the list that has the data not the word itself
+                        solutionList = self.dataExtractor(myStringList[currentCounter + stringOffSet:])
+
+                        break
+                currentCounter += 1
+
+
+        # Checking fot Operating Cash Flow
+        elif (dataType == Attribute.OPERATING_CASH_FLOW):
+            # The purpose of this offset is when we send the list to another method
+            # we want to make sure that the other method doesn't receive the word inside the list
+            stringOffSet = 20
+            currentCounter = 0
+            while (currentCounter < len(myStringList)):
+                character = "" + myStringList[currentCounter]
+                if (character.isupper()):
+
+                    if (self.operatingCashFlowStringChecker(myStringList[currentCounter:])):
+                        # Only sending the part of the list that has the data not the word itself
+                        solutionList = self.dataExtractor(myStringList[currentCounter + stringOffSet:])
+
+                        break
+                currentCounter += 1
+
+
+        # Checking fot Capital Expenditure
+        elif (dataType == Attribute.CAPITAL_EXPENDITURE):
+            # The purpose of this offset is when we send the list to another method
+            # we want to make sure that the other method doesn't receive the word inside the list
+            stringOffSet = 24
+            currentCounter = 0
+            while (currentCounter < len(myStringList)):
+                character = "" + myStringList[currentCounter]
+                if (character.isupper()):
+
+                    if (self.capitalExpenditureStringChecker(myStringList[currentCounter:])):
+                        # Only sending the part of the list that has the data not the word itself
+                        solutionList = self.dataExtractor(myStringList[currentCounter + stringOffSet:])
+
+                        break
+                currentCounter += 1
+
+        # Checking if the list is empty
+        if (len(solutionList) == None):
+            TypeError: "The solution list is empty or has not been filled in correctly"
+
+        return solutionList
+
+    def freeCashFlowStringChecker(self, theList):
+        """
+        This method verifies that the passed in string has the correct title.
+        The list passed into the method has to start at the word otherwise this method will declare it wrong.
+        :param theList:
+        :return: Boolean
+        """
+        if (len(theList) == 0):
+            TypeError: "The passed in list is empty"
+
+        # Assuming the list starts at the word
+        myString = "".join(theList[:14])
+
+        return (myString == "Free Cash Flow")
+
+    def operatingCashFlowStringChecker(self, theList):
+        """
+            This method verifies that the passed in string has the correct title.
+            The list passed into the method has to start at the word otherwise this method will declare it wrong.
+            :param theList:
+            :return: Boolean
+            """
+
+        if (len(theList) == 0):
+            TypeError: "The passed in list is empty"
+
+        # Assuming the list starts at the word
+        myString = "".join(theList[:19])
+
+        return (myString == "Operating Cash Flow")
+
+    def capitalExpenditureStringChecker(self, theList):
+        """
+            This method verifies that the passed in string has the correct title.
+            The list passed into the method has to start at the word otherwise this method will declare it wrong.
+            :param theList:
+            :return: Boolean
+            """
+
+        if (len(theList) == 0):
+            TypeError: "The passed in list is empty"
+
+        # Assuming the list starts at the word
+        myString = "".join(theList[:19])
+
+        return (myString == "Capital Expenditure")
+
+    def dataExtractor(self, theList):
+        """
+        This method extracts the data from the passed in list, please make sure
+        that the list that is passed does not start with any word like the header of the
+        data being extracted. This method will take care of empty space between the word and the numbers.
+        Only data from 1 line will be extracted.
+        :param theList:
+        :return:
+        """
+        counter = 0
+        trueStartIndex = 0
+        whiteSpace = True
+
+        for element in theList:
+            s = "" + element
+            if (element != "\n"):
+                # Checking if the numbers have white space in front of them
+                # Another way of checking for just the number and the white space
+                # The element can also contain a "-"
+                if ((s.isdigit() or s == "-") and whiteSpace):
+                    trueStartIndex = counter
+                    whiteSpace = False
+                counter += 1
+            else:
+                break
+
+        # Converting the strings into Integers
         mylist = ""
-        mylist = mylist.join((myStringList[freeAt + stringOffSet:]))
+        mylist = mylist.join((theList[trueStartIndex: counter]))
         mylist = mylist.split(" ")
+
         counter = 0
         while (counter < len(mylist)):
-            print(mylist[counter])
+            stringNumber = mylist[counter].replace(",", "")
+            mylist[counter] = int(stringNumber)
             counter += 1
+
         return mylist
 
 
 def main():
-    scraper = Scraper("TSLA")
-    scraper.freeCashFlow()
+    scraper = Scraper("AAPL")
+    scraper.revenue()
 
 main()
